7b7d1546831ed0df2915800b2489e5b0d9347fae
francescoPagliaPersonal <fra.py.study@gmail.com>
2026-01-29 14:57:01 +0100

    start of ex01

---
+++ cpp00/ex01/PhoneBook.class.hpp
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook.class.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fpaglia <fpaglia@student.42vienna.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/29 14:45:38 by fpaglia           #+#    #+#             */
+/*   Updated: 2026/01/29 14:51:34 by fpaglia          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHONEBOOK_CLASS_H
+# define PHONEBOOK_CLASS_H
+
+# include <iostream>
+# include <string>
+
+#endif
