b7674c0c2f8860accbf2e3cd23779df0fa6b12c2
fpaglia <fpaglia@student.42vienna.com>
2026-02-03 13:28:36 +0100

    changed megaphone functions (non allowed)

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -6,14 +6,13 @@
 /*   By: fpaglia <fpaglia@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/26 16:56:40 by fpaglia           #+#    #+#             */
-/*   Updated: 2026/01/29 13:36:50 by fpaglia          ###   ########.fr       */
+/*   Updated: 2026/02/03 13:27:28 by fpaglia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <locale>
 #include <string>
-#include <algorithm>
-#include <cctype>
 
 std::string set_uppercase(const char *str)
 {
@@ -21,17 +20,23 @@ std::string set_uppercase(const char *str)
 	if (!str)
 		return ("");
 
-	std::string strUpp(str);
-	std::transform(strUpp.begin(), strUpp.end(), strUpp.begin(), (int(*)(int))std::toupper);
+	std::locale		loc;
+	std::string	strUpp(str);
+	std::size_t	i = 0;
+	while (i < strUpp.length())
+	{
+		strUpp[i] = std::toupper(strUpp[i], loc);
+		i++;
+	}
 	return (strUpp);
 }
 
 int main(int ac, char **av)
 {
 	if (ac == 1)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 
-	for (int i = 1; i < ac; i++){
+	for (int i = 1; i < ac; ++i){
 		
 		std::cout << set_uppercase(av[i]);
 	}
