c5635e7afd579d57ac65d2d68ae19c147b6f24a2
fpaglia <fpaglia@student.42vienna.com>
2026-02-04 16:17:30 +0100

    modified the contact class to respect the ownership of the data

--- cpp00/ex01/Contact.class.cpp
+++ cpp00/ex01/Contact.class.cpp
@@ -6,13 +6,28 @@
 /*   By: fpaglia <fpaglia@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/29 19:00:13 by fpaglia           #+#    #+#             */
-/*   Updated: 2026/02/04 14:03:14 by fpaglia          ###   ########.fr       */
+/*   Updated: 2026/02/04 16:15:14 by fpaglia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Contact.class.hpp"
+#include <string>
 
 Contact::Contact(void) : name(""), surn(""), nick(""), phon(""), secr("") {}
 
 Contact::~Contact(void) {}
 
+std::string Contact::get_field(enum e_fields id) const
+{
+	if (id < MAX_FIELD)
+		return (param[id]);
+	return ("");
+}
+
+bool	Contact::set_field(enum e_fields id, std::string& str)
+{
+	if (id < 0 || id > MAX_FIELD)
+		return (false);
+	param[id].assign(str);
+	return (true)
+}
--- cpp00/ex01/Contact.class.hpp
+++ cpp00/ex01/Contact.class.hpp
@@ -6,7 +6,7 @@
 /*   By: fpaglia <fpaglia@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/29 15:36:18 by fpaglia           #+#    #+#             */
-/*   Updated: 2026/01/29 17:47:50 by fpaglia          ###   ########.fr       */
+/*   Updated: 2026/02/04 16:12:29 by fpaglia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,26 @@
 class Contact {
 
 	public:
+
+	enum e_fields
+	{
+		NAME,
+		SURNAME,
+		NICK,
+		PHONE,
+		SECRET,
+		MAX_FIELD
+	};
 	
 	Contact(void);
 	~Contact(void);
 
+	std::string	get_field(enum e_fields id) const;
+	bool		set_field(enum e_fields id, std::string& str);
+	
+	private:
+	
+	std::string param[MAX_FIELD];
 	std::string	name;
 	std::string	surn;
 	std::string	nick;
