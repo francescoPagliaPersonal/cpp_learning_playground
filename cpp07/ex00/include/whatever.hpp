fb6078af55289d464ec4bb103d5a634f4e08a26a
fpaglia <fpaglia@student.42vienna.com>
2026-04-16 10:35:28 +0200

    cpp07 ex00 included const qualifier to the param and the output where made sense

--- cpp07/ex00/include/whatever.hpp
+++ cpp07/ex00/include/whatever.hpp
@@ -6,7 +6,7 @@
 /*   By: fpaglia <fpaglia@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/15 19:19:44 by fpaglia           #+#    #+#             */
-/*   Updated: 2026/04/16 09:24:59 by fpaglia          ###   ########.fr       */
+/*   Updated: 2026/04/16 10:31:12 by fpaglia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,20 @@
 # define WHATEVER_H
 
 template<typename T>
-void	swap(T& a, T& b) {
+inline void	swap(T& a, T& b) {
 	T temp = a;
 	a = b;
 	b = temp;
 }
 
 template<typename T>
-T	min(T& a, T& b) {
+inline const T&	min(const T& a, const T& b) {
 	return a < b ? a : b;
 }
 
 template<typename T>
-T	max(T& a, T& b) {
+inline const T&	max(const T& a, const T& b) {
 	return a > b ? a : b;
 }
+
 #endif 
--- cpp07/ex00/source/main.cpp
+++ cpp07/ex00/source/main.cpp
@@ -6,24 +6,25 @@
 /*   By: fpaglia <fpaglia@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/15 19:21:13 by fpaglia           #+#    #+#             */
-/*   Updated: 2026/04/16 09:38:07 by fpaglia          ###   ########.fr       */
+/*   Updated: 2026/04/16 10:29:17 by fpaglia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "whatever.hpp"
+#include <algorithm>
 #include <iostream>
-#include <ostream>
 
 class A {
 	public: 
 	A(int num) : _num(num) {}; 
 	A(const A& obj) : _num(obj._num) {};
-	A& operator>(A& obj) {return _num > obj._num ? *this : obj; } 
-	A& operator<(A& obj) {return _num < obj._num ? *this : obj; } 
+	A& operator=(const A& obj) {if (this != &obj) _num = obj._num; return *this;} 
+	bool operator>(const A& obj) const {return _num > obj._num; } 
+	bool operator<(const A& obj) const {return _num < obj._num; } 
 	int getValue() const {return _num;}
 	
 	private: 
-	const int _num;
+	int _num;
 };
 
 std::ostream& operator<<(std::ostream& stream, const A& obj) {
@@ -58,5 +59,16 @@ int main(void)
 	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
 	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
 	}
+	{
+	float a = 7.11f;
+	float b = 6.43f;
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "min( a, b ) = " << std::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	}
 	return 0;
 }
+
