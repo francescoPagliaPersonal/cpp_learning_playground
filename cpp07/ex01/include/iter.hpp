237c264762361286271b7564f13e56aaad7b9d6c
fpaglia <fpaglia@student.42vienna.com>
2026-04-20 11:56:51 +0200

    cpp07 ex01 polished test for clarity of presentation, added further documentation in the code and reintroduced std::size_t

--- cpp07/ex01/include/iter.hpp
+++ cpp07/ex01/include/iter.hpp
@@ -6,35 +6,46 @@
 /*   By: fpaglia <fpaglia@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/16 11:08:02 by fpaglia           #+#    #+#             */
-/*   Updated: 2026/04/16 13:16:34 by fpaglia          ###   ########.fr       */
+/*   Updated: 2026/04/20 11:54:41 by fpaglia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ITER_H
 # define ITER_H
 
+#include <cstddef>
 
-typedef unsigned long size_t;
-
-
+/* DISCLOSURE NOTE OVER THE NOT PROTECTION OF THE NULL ARRAY.
+ *
+ * this being a template doesn't have knowledge about the caller
+ * any early termination creates a discrimant of the potential caller.
+ * what if I want to have null values in my array? 
+ * Imagine a vector of object that might need to be destroyed and replaced with a 
+ * valid NULL value as an example.
+ */
 template<typename T>
-void iter(T *arr, const size_t len, void (*f)(T const &))
+void iter(T *arr, const std::size_t len, void (*f)(T const &))
 {
-	for (size_t i = 0; i < len; ++i)
+	for (std::size_t i = 0; i < len; ++i)
 		f(arr[i]);				
 }
 
 template<typename T>
-void iter(T *arr, const size_t len, void (*f)(T &))
+void iter(T *arr, const std::size_t len, void (*f)(T &))
 {
-	for (size_t i = 0; i < len; ++i)
+	for (std::size_t i = 0; i < len; ++i)
 		f(arr[i]);				
 }
 
+/* This version could substitute completely the previous two 
+ * which I leave in here because of my understanding of the exercise requirements.
+ * It is interesting to see because in future c++(11+) the second Type 
+ * allows the usage of labda operator that otherwise wouldn't be possible. 
+ */
 template<typename T, typename A>
-void iter(T *arr, const size_t len, A f)
+void iter(T *arr, const std::size_t len, A f)
 {
-	for (size_t i = 0; i < len; ++i)
+	for (std::size_t i = 0; i < len; ++i)
 		f(arr[i]);				
 }
 
--- cpp07/ex01/source/main.cpp
+++ cpp07/ex01/source/main.cpp
@@ -6,45 +6,56 @@
 /*   By: fpaglia <fpaglia@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/16 11:42:59 by fpaglia           #+#    #+#             */
-/*   Updated: 2026/04/16 13:19:31 by fpaglia          ###   ########.fr       */
+/*   Updated: 2026/04/20 11:48:16 by fpaglia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iter.hpp"
 #include <cctype>
+#include <cstddef>
 #include <iostream>
 
+static int id = 0;
+static std::string msg;
 
 class Yes { 
+	
 	public:
-	Yes(void) : _num(0) {};
-	Yes(int num) : _num(num) {}; 
-	Yes(Yes const & obj) : _num(obj._num) {};
+	Yes(void)		: _num(0)	{};
+	Yes(int num)	: _num(num)	{}; 
+	Yes(Yes const & obj)
+					: _num(obj._num) {};
 	~Yes(void) {};
-	void	setNum(int num) {_num = num;};
-	int		getNum(void) const {return _num;};
+	Yes&	operator++(void) { _num += 1; return *this; }
+	
+	void	setNum(int num)		{_num = num;};
+	int		getNum(void) const	{return _num;};
+	
 	
 	private: 
 	int _num;
 };
 
-void add(int & num)
+void addInt(int & num)
 {
-	num += 5;
+	num += 10;
 }
 void addYes(Yes & obj)
 {
-	obj.setNum(5);
+	obj.setNum(obj.getNum() + 5);
 }
 
 void print(int const & num)
 {
-	std::cout << num << std::endl;
+	std::cout << "[" << id << "] " << msg << ": "  << num << std::endl;
+	id++;
 }
 
 void printYes(Yes const & obj) {
-	std::cout << obj.getNum() << std::endl;
+	std::cout << "[" << id << "] " << msg << ": " << obj.getNum() << std::endl;
+	id++;
 }
+
 int uppercase(char & c )
 {
 	int a = static_cast<int>(c);
@@ -55,21 +66,56 @@ int uppercase(char & c )
 void increment(int & x) { x++;}
 int increment(Yes & x) { x.setNum(x.getNum() * 2); return 1;}
 
-#define MYCLASS_SIZE 10
+#define MYCLASS_SIZE 4
 
 int main(void)
 {
-	int num[] = {0,1,2,3,4};
-	Yes myClass[MYCLASS_SIZE] ;
-	char str[] = "ciaobello!";
-	::iter(num, sizeof(num)/sizeof(num[0]), print);
-	::iter(num, sizeof(num)/sizeof(num[0]), add);
-	::iter(num, sizeof(num)/sizeof(num[0]), print);
-	::iter(str, sizeof(str)/sizeof(str[0]), uppercase);
-	std::cout << str << std::endl;
-	::iter(myClass, MYCLASS_SIZE,  addYes);
-	::iter(myClass, MYCLASS_SIZE,  static_cast<int(*)(Yes&)>(increment));
-	::iter(myClass, MYCLASS_SIZE, printYes);
-	::iter(num, sizeof(num)/sizeof(num[0]), print);
+	{
+		std::cout << "\n========== T1 INT ARRAY =============\n" << std::endl;
+		
+		int num[] = {0,1,2,3,4};
+		id = 0; msg.assign("source");
+		::iter(num, sizeof(num)/sizeof(num[0]), print); 
+		::iter(num, sizeof(num)/sizeof(num[0]), addInt);
+		id = 0; msg.assign("add 10");
+		::iter(num, sizeof(num)/sizeof(num[0]), print);
+	}
+	{
+		std::cout << "\n========== T2 MY CLASS ARRAY =============\n" << std::endl;
 
+		Yes myClass[MYCLASS_SIZE] ;
+		for (std::size_t i = 0; i < MYCLASS_SIZE; ++i)
+			myClass[i].setNum(i * 2);
+		id = 0; msg.assign("source");
+		::iter(myClass, MYCLASS_SIZE, printYes);
+		::iter(myClass, MYCLASS_SIZE,  addYes);
+		id = 0; msg.assign("add  5");
+		::iter(myClass, MYCLASS_SIZE, printYes);
+		::iter(myClass, MYCLASS_SIZE,  static_cast<int(*)(Yes&)>(increment));
+		id = 0; msg.assign("double");
+		::iter(myClass, MYCLASS_SIZE, printYes);
+		
+	}
+	{
+		std::cout << "\n========== T3 SINGLE STRING ARRAY =============\n" << std::endl;
+		
+		char str[] = "ciaobello!";
+		std::cout << "source  : " <<str << std::endl;
+		::iter(str, sizeof(str)/sizeof(str[0]), uppercase);
+		std::cout << "toupper : " << str << std::endl;
+	}
+	
+	{
+		/*
+		// requires the -std=c++11 in the make file for showcasing purposes
+		std::cout << "\n========== T4 LAMBDA FUNCTION C++11(+) ONLY =============\n" << std::endl;
+		
+		int num[] = {0,1,2,3,4};
+		id = 0; msg.assign("source");
+		::iter(num, sizeof(num)/sizeof(num[0]), print); 
+		::iter(num, sizeof(num)/sizeof(num[0]), [](int &a)->void{a *= 4;}); 
+		id = 0; msg.assign("labda ");
+		::iter(num, sizeof(num)/sizeof(num[0]), print); 
+		*/
+	}
 }
