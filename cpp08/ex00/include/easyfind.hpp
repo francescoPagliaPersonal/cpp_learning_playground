4f957b9f4a4c5c4fe95f806833e6ede0d225ede7
fpaglia <fpaglia@student.42vienna.com>
2026-04-21 12:44:48 +0200

    cpp08 ex00 included const_iterator specifier for a const container

--- cpp08/ex00/include/easyfind.hpp
+++ cpp08/ex00/include/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: fpaglia <fpaglia@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/20 15:56:15 by fpaglia           #+#    #+#             */
-/*   Updated: 2026/04/21 12:18:54 by fpaglia          ###   ########.fr       */
+/*   Updated: 2026/04/21 12:42:57 by fpaglia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 #include <errno.h>
 
 template<typename T>
-typename T::iterator	easyfind(T const & container, int value) 
+typename T::const_iterator	easyfind(T const & container, int value) 
 {
 	typename T::iterator it;
 	for (it = container.begin(); it != container.end(); ++it)
@@ -28,7 +28,7 @@ typename T::iterator	easyfind(T const & container, int value)
 
 
 template<typename T>
-typename T::iterator	easyfind(T & container, int value) 
+typename T::iterator		easyfind(T & container, int value) 
 {
 	typename T::iterator it;
 	for (it = container.begin(); it != container.end(); ++it)
