90e24a22662a27d8b566476064a6af14f86b63ff
fpaglia <fpaglia@student.42vienna.com>
2026-04-23 10:57:35 +0200

    cpp08 ex01 completed test execution

--- cpp08/ex01/source/main.cpp
+++ cpp08/ex01/source/main.cpp
@@ -6,7 +6,7 @@
 /*   By: fpaglia <fpaglia@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/22 11:40:24 by fpaglia           #+#    #+#             */
-/*   Updated: 2026/04/22 15:57:41 by fpaglia          ###   ########.fr       */
+/*   Updated: 2026/04/23 10:55:17 by fpaglia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,25 +19,34 @@ int main(void)
 {
 	Span myobj(5000);
 	Span smallObj(5);
+	int counter = 0;
 	double arr[] = {1.5,2.3,3.1,4.9,5.6,6.4,7.3,8.2,9.1,10.45};
 	int arr1[] = {1,2,3,4,5,6,7,8,9,105};
 	
 	{
+		std::cout << "\n\n========== T" << counter << " THROW DATA TYPE =============\n" << std::endl; ++counter;
+
 		try { myobj.addRange(&arr[0], &arr[9] ); }
 		catch (std::exception & e) 
 			{ std::cout << "ERROR: " << e.what() << std::endl; }
 	}	
 	{
+		std::cout << "\n\n========== T" << counter << " THROW WRONG RANGE =============\n" << std::endl; ++counter;
+		
 		try { myobj.addRange(&arr1[9], &arr1[6] ); }
 		catch (std::exception & e) 
 			{ std::cout << "ERROR: " << e.what() << std::endl; }
 	}
 	{
+		std::cout << "\n\n========== T" << counter << " THROW OUT OF RANGE =============\n" << std::endl; ++counter;
+
 		try { smallObj.addRange(&arr1[0], &arr1[9] ); }
 		catch (std::exception & e) 
 			{ std::cout << "ERROR: " << e.what() << std::endl; }
 	}
 	{
+		std::cout << "\n\n========== T" << counter << " THROW NOT ENOUGH ITEMS =============\n" << std::endl; ++counter;
+		
 		smallObj.addNumber(84);
 		try { smallObj.longestSpan();}
 		catch (std::exception & e) 
@@ -47,7 +56,41 @@ int main(void)
 		catch (std::exception & e) 
 			{ std::cout << "ERROR: " << e.what() << std::endl; }
 	}
+	{
+		std::cout << "\n\n========== T" << counter << " THROW TOO MANY NUMBERS =============\n" << std::endl; ++counter;
+
+		Span sp = Span(3);
+		try {
+			sp.addNumber(6);
+			sp.addNumber(3);
+			sp.addNumber(17);
+			sp.addNumber(9);
+			sp.addNumber(11);
+			std::cout 
+					<< "\ninput data:    " << sp
+					<< "\nshortest span: " << sp.shortestSpan()
+					<< "\nlongest span : " << sp.longestSpan() << std::endl;
+		}
+		catch (std::exception & e) 
+			{ std::cout << "ERROR: " << e.what() << std::endl; }
+	}
+	{
+		std::cout << "\n\n========== T" << counter << " ADD NUMBERS =============\n" << std::endl; ++counter;
+
+		Span sp = Span(5);
+		sp.addNumber(6);
+		sp.addNumber(3);
+		sp.addNumber(17);
+		sp.addNumber(9);
+		sp.addNumber(11);
+		std::cout 
+				<< "\ninput data:    " << sp
+				<< "\nshortest span: " << sp.shortestSpan()
+				<< "\nlongest span : " << sp.longestSpan() << std::endl;
+	}
 	{	
+		std::cout << "\n\n========== T" << counter << " ADD RANGE =============\n" << std::endl; ++counter;
+		
 		std::list<int> test;
 		for (int j = 1; j < 900; j +=2) {
 			if (j % 3 != 0) {
@@ -63,22 +106,10 @@ int main(void)
 		myobj.addNumber(46);
 		myobj.addRange(test.begin(), test.end());
 		std::cout 
-				<< "\nInput deque:   " << test.size() 
+				<< "\nInput deque:   " << test.size() << "items"
 				<< "\nshortest span: " << myobj.shortestSpan() 
 				<< "\nlongest span : " << myobj.longestSpan() << std::endl;
 		
 	}
-	{
-		Span sp = Span(5);
-		sp.addNumber(6);
-		sp.addNumber(3);
-		sp.addNumber(17);
-		sp.addNumber(9);
-		sp.addNumber(11);
-		std::cout 
-				<< "\ninput data:    " << sp
-				<< "\nshortest span: " << sp.shortestSpan()
-				<< "\nlongest span : " << sp.longestSpan() << std::endl;
-	}
-	return 0;	
+return 0;	
 }
