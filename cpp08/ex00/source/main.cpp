2d531ab5fca0dd2681e59f8f45a0d8b0f0694e98
fpaglia <fpaglia@student.42vienna.com>
2026-04-24 10:54:09 +0200

    cpp08 ex00 included check against standard find

--- cpp08/ex00/source/main.cpp
+++ cpp08/ex00/source/main.cpp
@@ -6,11 +6,12 @@
 /*   By: fpaglia <fpaglia@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/21 11:48:47 by fpaglia           #+#    #+#             */
-/*   Updated: 2026/04/21 12:45:45 by fpaglia          ###   ########.fr       */
+/*   Updated: 2026/04/24 10:53:27 by fpaglia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "easyfind.hpp"
+#include <algorithm>
 #include <deque>
 #include <list>
 #include <cstddef>
@@ -23,7 +24,8 @@ int main(void)
 	const int	num[] = {0, 2, 4, 8, 16, 32, 64, 128 };
 	
 	{
-		std::cout << "\n\n========== T1 LIST =============\n" << std::endl;
+		
+		std::cout << "\n\n========== T0 std::find =============\n" << std::endl;
 
 		
 		std::list<int> contOfNum;
@@ -31,18 +33,39 @@ int main(void)
 
 		for (std::size_t i = 0; i < sizeof(num)/sizeof(num[0]); ++i)
 			contOfNum.push_back(num[i]);
-		
-		
+	
+		it = std::find(contOfNum.begin(), contOfNum.end(), 32);
+		if (it  != contOfNum.end())
+			std::cout << "std::find     :" << *it << std::endl;
+		else
+			std::cout << "no item found, error no: "<< errno << std::endl;	
+	
+		errno = 0;
+
+		it = std::find(contOfNum.begin(), contOfNum.end(), 33);
+		if (it  != contOfNum.end())
+			std::cout << "std::find     :" << *it << std::endl;
+		else
+			std::cout << "no item found, error no: "<< errno << std::endl;	
+	
+		std::cout << "\n\n========== T1 LIST =============\n" << std::endl;
+	
 		it = ::easyfind(contOfNum, 32);
 		if (it != contOfNum.end())
-			std::cout << *it << std::endl;
+			std::cout << "easyfind      :" << *it << std::endl;
 		else
 			std::cout << "no item found, error no: "<< errno << std::endl;	
+		
+		
 		it = ::easyfind(contOfNum, 33);
 		if (it != contOfNum.end())
-			std::cout << *it << std::endl;
+			std::cout << "easyfind      :" << *it << std::endl;
 		else
 			std::cout << "no item found, error no: "<< errno << std::endl;	
+		
+		errno = 0;
+		std::cout << "\nerrno resetted: "<< errno << std::endl;
+		
 	}
 	{
 		std::cout << "\n\n========== T2 VECTOR =============\n" << std::endl;
@@ -56,15 +79,20 @@ int main(void)
 		
 		it = ::easyfind(contOfNum, 32);
 		if (it != contOfNum.end())
-			std::cout << *it << std::endl;
+			std::cout << "easyfind      :" << *it << std::endl;
 		else
 			std::cout << "no item found, error no: "<< errno << std::endl;	
+
+		errno = 0;
+		
 		it = ::easyfind(contOfNum, 33);
 		if (it != contOfNum.end())
-			std::cout << *it << std::endl;
+			std::cout << "easyfind      :" << *it << std::endl;
 		else
 			std::cout << "no item found, error no: "<< errno << std::endl;		
 		
+		errno = 0;
+		std::cout << "\nerrno resetted: "<< errno << std::endl;
 	}
 	{
 		std::cout << "\n\n========== T3 DEQUE =============\n" << std::endl;
@@ -78,12 +106,15 @@ int main(void)
 		
 		it = ::easyfind(contOfNum, 32);
 		if (it != contOfNum.end())
-			std::cout << *it << std::endl;
+			std::cout << "easyfind      :" << *it << std::endl;
 		else
 			std::cout << "no item found, error no: "<< errno << std::endl;	
+
+		errno = 0;
+
 		it = ::easyfind(contOfNum, 33);
 		if (it != contOfNum.end())
-			std::cout << *it << std::endl;
+			std::cout << "easyfind      :" << *it << std::endl;
 		else
 			std::cout << "no item found, error no: "<< errno << std::endl;	
 		
