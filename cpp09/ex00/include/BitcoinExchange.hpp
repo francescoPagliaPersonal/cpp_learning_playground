4932796aa1bbe55115320b12ad5b91c564d0206c
fpaglia <fpaglia@student.42vienna.com>
2026-04-28 15:30:25 +0200

    cpp09 ex00 db is filling up properly but code is very ugly... to be refactored

--- cpp09/ex00/include/BitcoinExchange.hpp
+++ cpp09/ex00/include/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: fpaglia <fpaglia@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/27 12:10:34 by fpaglia           #+#    #+#             */
-/*   Updated: 2026/04/28 11:54:12 by fpaglia          ###   ########.fr       */
+/*   Updated: 2026/04/28 15:29:23 by fpaglia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,6 +42,11 @@ class BitcoinExchange {
 		
 	static bool			_checkHeader(std::fstream & in, const std::string & title);
 	static std::string	_extractDate(const std::string & date);
+	static bool			_followsValidDelimiter(const std::string & source, const std::string & pattern);
+	static double 		_extractRate(const std::string & source, int offset);
+/*	bool 				_parseLine(const std::string & line, const std::string & delimiter,
+									std::string & date, double & rate, std::string & error);
+*/	
 };
 #endif
 
--- cpp09/ex00/source/BitcoinExchange.init.cpp
+++ cpp09/ex00/source/BitcoinExchange.init.cpp
@@ -6,7 +6,7 @@
 /*   By: fpaglia <fpaglia@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/28 09:26:51 by fpaglia           #+#    #+#             */
-/*   Updated: 2026/04/28 11:50:04 by fpaglia          ###   ########.fr       */
+/*   Updated: 2026/04/28 15:18:31 by fpaglia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,37 @@
 #include <cctype>
 #include <cstdlib>
 #include <cerrno>
-#include <memory>
-#include <stdexcept>
+// #include <stdexcept>
 
+bool BitcoinExchange::_followsValidDelimiter(const std::string & source, const std::string & pattern)
+{
+	std::string delimiter = source.substr(DATE_LEN, pattern.size());
+	return delimiter == pattern ? true : false;
+}
+
+double BitcoinExchange::_extractRate(const std::string & source, int offset) 
+{
+	std::string value = source.substr(offset, source.size());
+	
+	char *end;
+	errno = 0;
+	double number = std::strtod(value.c_str(), &end);
+	if (value.c_str() == end)
+		return (errno = ERANGE, -1.0);
+	if (errno == ERANGE)
+		return -1.0;
+	if (*end != 0)
+	{
+		std::cout << "value of end: '" << *end << "'" <<std::endl;
+		while (*end != 0)
+		{
+			if (!std::isspace(*end))
+				return (errno = EINVAL, -1.0);
+			++end;
+		}
+	}
+	return number;
+}
 
 
 void BitcoinExchange::init(void) 
@@ -31,38 +59,28 @@ void BitcoinExchange::init(void)
 	{
 		std::string myline;
 		std::string date;
-		std::string rate;
+		double		rate;
+		std::string delimiter(",");
+		
 		std::getline(tmp, myline);
 		if (myline.size() == 0)
 			break;
+		
 		date = _extractDate(myline);
 		if (date.size() == 0)
 			throw std::runtime_error("Invalid entry - wrong date format.");
 		
-		std::string pattern(",");
-		std::string delimiter = myline.substr(DATE_LEN, pattern.size());
-		if (delimiter != pattern)
+		if (!_followsValidDelimiter(myline, ","))
 			throw std::runtime_error("Invalid entry - wrong delimiter.");
+				
 		
-		
-		rate = myline.substr(DATE_LEN + pattern.size(), myline.size());
-
-		char *end;
-		errno = 0;
-		double number = std::strtod(rate.c_str(), &end);
-		if (errno != 0)
+		rate = _extractRate(myline, DATE_LEN + delimiter.size());
+		if (errno == ERANGE)
 			throw std::runtime_error("Invalid entry - wrong value format.");
-		if (*end != 0)
-		{
-			std::cout << "value of end: '" << *end << "'" <<std::endl;
-			while (*end != 0)
-			{
-				if (!std::isspace(*end))
-					throw std::runtime_error("Invalid entry - unexpected character in entry.");
-				++end;
-			}
-		}
-		_dbBTC.insert(std::pair<std::string, double>(date, number));
+		if (errno == EINVAL)
+			throw std::runtime_error("Invalid entry - unexpected character in entry.");
+		
+		_dbBTC.insert(std::pair<std::string, double>(date, rate));
 		
 	}
 	std::map<std::string, double>::const_iterator mapIt = _dbBTC.begin();
@@ -70,3 +88,11 @@ void BitcoinExchange::init(void)
 		std::cout << mapIt->first << "::" << mapIt->second << std::endl;
 	std::cout << "items in map: "<< _dbBTC.size() << std::endl;
 }
+
+/*
+std::string date;
+double rate;
+std::string err;
+if (!parseLine(line, ",", date, rate, err))
+    throw std::runtime_error(err);
+*/
