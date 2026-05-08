5edb8f8615fb91bddbcf57ba8e893b5018f0fa5a
fpaglia <fpaglia@student.42vienna.com>
2026-05-08 09:29:16 +0200

    training removed old implementation of the find index

--- training/mergeInsert/findIndex.cpp
+++ training/mergeInsert/findIndex.cpp
@@ -1,52 +1,6 @@
 #include "header.hpp"
 #include <ostream>
 
-// static void statistics(std::vector<node_s *> & arr, idx_id start, idx_id middle, idx_id end)
-// {
-// 	std::cout << "DEBUG: " << "[" <<  start << "] " << arr[start]->value ;
-// 	std::cout << "  [" <<  end   << "] " << arr[end]->value  ;
-// 	std::cout << "  [" <<  middle   << "] " << arr[middle]->value   << std::endl;
-// }
-
-// ptr_id	findIndex(std::vector<node_s *> & arr, int value)
-// {
-// 	ptr_id start = 0;
-// 	ptr_id end = arr.size() - 1;
-// 	ptr_id middle = (end - start) /2;
-	
-// 	if (DEBUG >= 2) {
-// 		std::cout << "DEBUG: " << "FIND INDEX for value: " << value << std::endl;
-// 		printVectorNode_s("DEBUG: input array: ", arr);
-// 		statistics(arr, start, middle, end);
-// 	}
-
-// 	while (end - start != 1)
-// 	{
-// 		if (value > arr[middle]->value)
-// 			start = middle;
-// 		else 
-// 			end = middle;
-// 		middle = start + ((end - start) /2);
-// 		__counter++;
-// 		if (DEBUG >= 2)
-// 			statistics(arr, start, middle, end);
-// 	}
-
-// 	if (value > arr[end]->value) {
-// 		middle = end + 1 ;
-// 		__counter++;
-// 	}
-// 	else {
-// 		middle = value > arr[start]->value ? start + 1: start;
-// 		__counter++;
-// 	}
-// 	if (DEBUG >= 2) {
-// 		std::cout << "DEBUG: " << " selected index: " << middle << std::endl;
-// 	}
-// 	return middle;
-// }
-
-
 ptr_id findIndex(std::vector<node_s *> & arr, int value) {
 	ptr_id low = 0;
 	ptr_id high = arr.size(); 
