fb4f0a1959d939b45b1502fa35544d4fa296fa5f
fpaglia <fpaglia@student.42vienna.com>
2026-05-08 15:26:00 +0200

    training update of the fordjhonson to use std::log

--- training/mergeInsert/data.cpp
+++ training/mergeInsert/data.cpp
@@ -3,8 +3,9 @@
 
 int fordJohnsonBound(int n) {
     double total = 0;
+    const double logTwo = std::log(2.0); 
     for (int k = 1; k <= n; ++k) {
-        total += std::ceil(log2(0.75 * k));
+        total += std::ceil(std::log(0.75 * k) / logTwo);
     }
     return static_cast<int>(total);
 }
